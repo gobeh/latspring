fa1b54a8ce38cfb0de8c86e9c76fe84fc4d557dc
gobeh <ahmad.sofwan@uinjkt.ac.id>
2019-05-20 13:32:27 +0700

    add basic login. data acquired via database

---
+++ src/main/java/com/sofwan/latspring/config/KonfigSecurity.java
@@ -0,0 +1,41 @@
+package com.sofwan.latspring.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class KonfigSecurity extends WebSecurityConfigurerAdapter {
+
+    private static final String SQL_LOGIN
+            ="select username, password, active as enabled " +
+            "from s_users where username= ?";
+    private static final String SQL_PERMISSION
+            ="select u.username, r.nama as authority " +
+            "from s_users u join s_users_roles ur on u.id = ur.id_users " +
+            "join s_roles r on ur.id_roles = r.id " +
+            "where u.username = ?";
+
+    @Autowired
+    DataSource ds;
+
+    @Autowired
+    public void konfigurGlobal(AuthenticationManagerBuilder auth) throws Exception{
+        auth
+                .jdbcAuthentication()
+                .dataSource(ds)
+                .usersByUsernameQuery(SQL_LOGIN)
+                .authoritiesByUsernameQuery(SQL_PERMISSION);
+
+              //  .inMemoryAuthentication()
+              //  .withUser("alan")
+              //  .password("{noop}123")
+              //  .roles("USER");
+    }
+}
---
+++ src/test/resources/data/login.sql
@@ -0,0 +1,52 @@
+drop table s_users;
+drop table s_roles;
+drop table s_users_roles;
+
+create table s_users(
+    id VARCHAR(35) PRIMARY KEY,
+    username VARCHAR(35) NOT NULL,
+    password varchar(255) NOT NULL,
+    active BOOLEAN
+);
+
+insert into s_users(id, username, password, active)
+values('u001','sofwan','123',true);
+
+insert into s_users(id, username, password, active)
+values('u002','fauzi','123',true);
+
+create table s_roles(
+    id VARCHAR(35) PRIMARY KEY,
+    nama varchar(255)
+);
+
+insert into s_roles(id, nama)
+values ('adm','ROLE_ADMIN');
+
+insert into s_roles(id, nama)
+values ('stf','ROLE_STAF');
+
+create table s_users_roles(
+    id_users VARCHAR(35) NOT NULL,
+    id_roles VARCHAR(35) NOT NULL
+);
+
+insert into s_users_roles(id_users, id_roles)
+values ('u001','adm');
+
+insert into s_users_roles(id_users, id_roles)
+values ('u001','stf')
+
+insert into s_users_roles(id_users, id_roles)
+values ('u002','stf');
+
+select username, password, active as enabled from s_users where username='sofwan';
+
+select u.username, r.nama as authority from s_users u join s_users_roles ur on u.id = ur.id_users
+join s_roles r on ur.id_roles = r.id where u.username = 'sofwan';
+
+
+
+
+
+
